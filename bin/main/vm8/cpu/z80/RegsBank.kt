183dd12dd1c276de83bedcd30b31e72bebaf540a
Alvaro Polo <apoloval@gmail.com>
2021-12-29 11:21:26 +0100

    Extend Z80 registers and include tests

--- bin/main/vm8/cpu/z80/RegsBank.kt
+++ bin/main/vm8/cpu/z80/RegsBank.kt
@@ -6,7 +6,17 @@ import vm8.data.*
 class RegsBank {
     var af: Word = Word(0x0000)
     var bc: Word = Word(0x0000)
+    var de: Word = Word(0x0000)
+    var hl: Word = Word(0x0000)
     var pc: Word = Word(0x0000)
+
+    var a: Octet 
+        inline get() = af.high()
+        inline set(v) { af = af.setHigh(v) }
+
+    var f: Octet 
+        inline get() = af.low()
+        inline set(v) { af = af.setLow(v) }
 
     var b: Octet 
         inline get() = bc.high()
@@ -15,4 +25,20 @@ class RegsBank {
     var c: Octet 
         inline get() = bc.low()
         inline set(v) { bc = bc.setLow(v) }
+
+    var d: Octet 
+        inline get() = de.high()
+        inline set(v) { de = de.setHigh(v) }
+
+    var e: Octet 
+        inline get() = de.low()
+        inline set(v) { de = de.setLow(v) }
+
+    var h: Octet 
+        inline get() = hl.high()
+        inline set(v) { hl = hl.setHigh(v) }
+
+    var l: Octet 
+        inline get() = hl.low()
+        inline set(v) { hl = hl.setLow(v) }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.0.3")
 }
 
 graalvmNative { 
