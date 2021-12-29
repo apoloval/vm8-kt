f4e74cbcfeede6c33d24c50f7905eae89e4a0b80
Alvaro Polo <apoloval@gmail.com>
2021-12-29 11:59:17 +0100

    Add tests for Z80 operands

--- bin/main/vm8/cpu/z80/Operands.kt
+++ bin/main/vm8/cpu/z80/Operands.kt
@@ -15,6 +15,14 @@ inline suspend fun Processor.load8(op: SrcOp8): Octet = with(op) { get() }
 inline suspend fun Processor.store8(op: DestOp8, v: Octet) { with(op) { set(v) } }
 
 enum class Reg8 : DestOp8 {
+    A {
+        override suspend fun Processor.get(): Octet = regs.a
+        override suspend fun Processor.set(v: Octet) { regs.a = v }
+    }, 
+    F {
+        override suspend fun Processor.get(): Octet = regs.f
+        override suspend fun Processor.set(v: Octet) { regs.f = v }
+    }, 
     B {
         override suspend fun Processor.get(): Octet = regs.b
         override suspend fun Processor.set(v: Octet) { regs.b = v }
@@ -22,7 +30,23 @@ enum class Reg8 : DestOp8 {
     C {
         override suspend fun Processor.get(): Octet = regs.c
         override suspend fun Processor.set(v: Octet) { regs.c = v }
-    };    
+    },
+    D {
+        override suspend fun Processor.get(): Octet = regs.d
+        override suspend fun Processor.set(v: Octet) { regs.d = v }
+    },    
+    E {
+        override suspend fun Processor.get(): Octet = regs.e
+        override suspend fun Processor.set(v: Octet) { regs.e = v }
+    },    
+    H {
+        override suspend fun Processor.get(): Octet = regs.h
+        override suspend fun Processor.set(v: Octet) { regs.h = v }
+    },    
+    L {
+        override suspend fun Processor.get(): Octet = regs.l
+        override suspend fun Processor.set(v: Octet) { regs.l = v }
+    },    
 }
 
 sealed interface SrcOp16 {
@@ -38,9 +62,21 @@ inline suspend fun Processor.load16(op: SrcOp16): Word = with(op) { get() }
 inline suspend fun Processor.store16(op: DestOp16, v: Word) { with(op) { set(v) } }
 
 enum class Reg16 : DestOp16 {
+    AF {
+        override suspend fun Processor.get(): Word = regs.af
+        override suspend fun Processor.set(v: Word) { regs.af = v }
+    }, 
     BC {
         override suspend fun Processor.get(): Word = regs.bc
         override suspend fun Processor.set(v: Word) { regs.bc = v }
+    }, 
+    DE {
+        override suspend fun Processor.get(): Word = regs.de
+        override suspend fun Processor.set(v: Word) { regs.de = v }
+    }, 
+    HL {
+        override suspend fun Processor.get(): Word = regs.hl
+        override suspend fun Processor.set(v: Word) { regs.hl = v }
     }, 
     PC {
         override suspend fun Processor.get(): Word = regs.pc
--- src/main/kotlin/vm8/cpu/z80/Bus.kt
+++ src/main/kotlin/vm8/cpu/z80/Bus.kt
@@ -5,6 +5,9 @@ import vm8.data.*
 
 typealias Addr = Word
 
+/**
+ * A bus where the Z80 processor is connected to.
+ */
 interface Bus {
     // Read a datum at given address from the bus
     suspend fun read(addr: Addr): Octet
@@ -16,4 +19,21 @@ interface Bus {
         read(addr), 
         read(addr.inc()),
     )
+}
+
+/**
+ * A bus that is disconnected from the system.
+ */
+object DisconnectedBus : Bus {
+    override suspend fun read(addr: Addr): Octet = Octet(0xFF)
+    override suspend fun write(addr: Addr, v: Octet) {}    
+}
+
+/**
+ * A bus that emulates a minimal Z80 system, mainly for testing purposes.
+ */
+class MinimalSystem : Bus {
+    val memory = ByteArray(64*1024)
+    override suspend fun read(addr: Addr): Octet = Octet(memory[addr.toInt()].toInt())
+    override suspend fun write(addr: Addr, v: Octet) { memory[addr.toInt()] = v.toByte() }
 }
